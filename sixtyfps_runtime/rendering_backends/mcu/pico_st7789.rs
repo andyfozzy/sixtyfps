b7dbd865a59063272f1962e87fd1b66b550922b8
Olivier Goffart <ogoffart@sixtyfps.io>
2022-01-20 23:01:07 +0100

    MCU: touch events are working now
    But they don't seem to have much effect yet

--- sixtyfps_runtime/rendering_backends/mcu/pico_st7789.rs
+++ sixtyfps_runtime/rendering_backends/mcu/pico_st7789.rs
@@ -1,14 +1,16 @@
 // Copyright © SixtyFPS GmbH <info@sixtyfps.io>
 // SPDX-License-Identifier: (GPL-3.0-only OR LicenseRef-SixtyFPS-commercial)
 
+extern crate alloc;
+
+use alloc::boxed::Box;
+pub use cortex_m_rt::entry;
+use embedded_hal::blocking::spi::Transfer;
+use embedded_hal::digital::v2::{InputPin, OutputPin};
 use embedded_time::rate::*;
 use rp_pico::hal;
 use rp_pico::hal::pac;
 use rp_pico::hal::prelude::*;
-
-pub use cortex_m_rt::entry;
-use embedded_hal::blocking::spi::Transfer;
-use embedded_hal::digital::v2::{InputPin, OutputPin};
 
 use defmt_rtt as _; // global logger
 use panic_probe as _;
@@ -47,6 +49,8 @@ pub fn init_board() {
 
     let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().integer());
 
+    unsafe { ALLOCATOR.init(&mut HEAP as *const u8 as usize, core::mem::size_of_val(&HEAP)) }
+
     let sio = hal::sio::Sio::new(pac.SIO);
 
     let pins = rp_pico::Pins::new(pac.IO_BANK0, pac.PADS_BANK0, sio.gpio_bank0, &mut pac.RESETS);
@@ -63,12 +67,14 @@ pub fn init_board() {
         4_000_000u32.Hz(),
         &embedded_hal::spi::MODE_3,
     );
+    // FIXME: a cleaner way to get a static reference, or be able to use non-static backend
+    let spi = Box::leak(Box::new(shared_bus::BusManagerSimple::new(spi)));
 
     let rst = pins.gpio15.into_push_pull_output();
 
     let dc = pins.gpio8.into_push_pull_output();
     let cs = pins.gpio9.into_push_pull_output();
-    let di = display_interface_spi::SPIInterface::new(spi, dc, cs);
+    let di = display_interface_spi::SPIInterface::new(spi.acquire_spi(), dc, cs);
 
     let mut display = st7789::ST7789::new(di, rst, 320, 240);
 
@@ -80,21 +86,14 @@ pub fn init_board() {
         bl.set_high().unwrap();
     }
 
-    unsafe { ALLOCATOR.init(&mut HEAP as *const u8 as usize, core::mem::size_of_val(&HEAP)) }
-
     display.init(&mut delay).unwrap();
     display.set_orientation(st7789::Orientation::Landscape).unwrap();
 
-    let touch_spi = hal::Spi::<_, _, 8>::new(pac.SPI0).init(
-        &mut pac.RESETS,
-        clocks.peripheral_clock.freq(),
-        4_000_000u32.Hz(),
-        &embedded_hal::spi::MODE_0,
-    );
+
     let touch = xpt2046::XPT2046::new(
         pins.gpio17.into_pull_down_input(),
         pins.gpio16.into_push_pull_output(),
-        touch_spi,
+        spi.acquire_spi(),
     )
     .unwrap();
 
@@ -126,12 +125,23 @@ impl<Display: Devices, IRQ: InputPin, CS: OutputPin<Error = IRQ::Error>, SPI: Tr
     }
 
     fn read_touch_event(&mut self) -> Option<sixtyfps_corelib::input::MouseEvent> {
-        self.touch.read().map_err(|_| ()).unwrap().map(|point| {
-            sixtyfps_corelib::input::MouseEvent::MousePressed {
-                pos: point.cast(),
+        self.touch
+            .read()
+            .map_err(|_| ())
+            .unwrap()
+            .map(|point| {
+                let point = point.to_f32() / (u16::MAX as f32);
+                let size = self.display.screen_size().to_f32();
+                sixtyfps_corelib::input::MouseEvent::MousePressed {
+                    pos: euclid::point2(point.x * size.width, point.y * size.height),
+                    button: sixtyfps_corelib::items::PointerEventButton::left,
+                }
+            })
+            // TODO: We should keep a state and only release once
+            .or(Some(sixtyfps_corelib::input::MouseEvent::MouseReleased {
+                pos: Default::default(),
                 button: sixtyfps_corelib::items::PointerEventButton::left,
-            }
-        })
+            }))
     }
 }
 
@@ -155,7 +165,7 @@ mod xpt2046 {
             Ok(Self { irq, cs, spi /*last: Default::default() */ })
         }
 
-        pub fn read(&mut self) -> Result<Option<Point2D<u16>>, Error<PinE, SPI::Error>> {
+        pub fn read(&mut self) -> Result<Option<Point2D<i16>>, Error<PinE, SPI::Error>> {
             if self.irq.is_low().map_err(|e| Error::Pin(e))? {
                 self.cs.set_low().map_err(|e| Error::Pin(e))?;
                 const CMD_X_READ: u8 = 0b10010000;
@@ -164,7 +174,7 @@ mod xpt2046 {
                 macro_rules! xchg {
                     ($byte:expr) => {
                         match self.spi.transfer(&mut [$byte]).map_err(|e| Error::Transfer(e))? {
-                            [x] => *x as u16,
+                            [x] => *x as i16,
                             _ => return Err(Error::InternalError),
                         }
                     };
@@ -178,7 +188,7 @@ mod xpt2046 {
 
                 self.cs.set_high().map_err(|e| Error::Pin(e))?;
 
-                Ok(Some(Point2D::new(x, y)))
+                Ok(Some(Point2D::new(y, i16::MAX - x)))
             } else {
                 Ok(None)
             }
